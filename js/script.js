de9275bf550d038a004bc114555bf04682023ac9
alessiodalberto002 <aledalberto002.com>
2025-12-24 15:09:41 +0100

    Creo una funzione che mostro il risultato del testo

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 function prendiTesto() {
   return document.getElementById("testo").value;
 }
+
+// MOSTRA IL RISULTATO //
+function mostraRisultato(testo) {
+  document.getElementById("risultato").textContent = testo;
+}
