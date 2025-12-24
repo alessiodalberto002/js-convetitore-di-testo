54e9b2b00bcd2f46a964163d06703f219dfd1d92
alessiodalberto002 <aledalberto002.com>
2025-12-24 15:12:12 +0100

    Creo una funzione che trasformi  le iniziali delle parole del testo in MAIUSCOLO

--- js/script.js
+++ js/script.js
@@ -19,3 +19,15 @@ function minuscolo() {
   let testo = prendiTesto();
   mostraRisultato(testo.toLowerCase());
 }
+
+// TRASFORMA LE INIZIALO DELLE PAROLE IN MAIUSCOLO //
+function inizialiMaiuscole() {
+  let testo = prendiTesto();
+
+  let parole = testo.split(" ");
+  let risultato = parole.map(parola =>
+    parola.charAt(0).toUpperCase() + parola.slice(1).toLowerCase()
+  );
+
+  mostraRisultato(risultato.join(" "));
+}
